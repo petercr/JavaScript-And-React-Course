2fb98f2514edd15f5d41f8cba78ac77c461c4a8b
Peter Cruckshank <petecruckshank@gmail.com>
2018-01-13 15:52:51 -0500

    Updated username to my user name & avitar

--- JSON Project/jsonhandling.js
+++ JSON Project/jsonhandling.js
@@ -1,9 +1,10 @@
-fetch('https://api.github.com/users/cassidoo')
+fetch('https://api.github.com/users/petercr')
   .then(function(r) {
     return r.json();
   })
   .then(function(j) {
-    console.log(j);
+    console.dir(j);
     var newStuff = document.getElementById('username');
     document.getElementById("image").src = j.avatar_url;
+    
   })
