fb37ad4d9aa3f0b1769c69d959f52a3b99531c62
Peter Cruckshank <petecruckshank@gmail.com>
2018-01-13 20:26:42 -0500

    now displays name, location, bio, and follwers # on page from JSON res

--- JSON Project/jsonhandling.js
+++ JSON Project/jsonhandling.js
@@ -1,10 +1,15 @@
 fetch('https://api.github.com/users/petercr')
-  .then(function(r) {
-    return r.json();
-  })
-  .then(function(j) {
-    console.dir(j);
-    var newStuff = document.getElementById('username');
-    document.getElementById("image").src = j.avatar_url;
-    
-  })
+    .then(function(r) {
+        return r.json();
+    })
+    .then(function(j) {
+        console.dir(j);
+        var newStuff = document.getElementById('username');
+        document.getElementById("image").src = j.avatar_url;
+        newStuff.innerHTML += "Real Name: " + j.name + "</br>";
+        newStuff.innerHTML += "Location: " + j.location + "</br>";
+        newStuff.innerHTML += "Bio: " + j.bio + "</br>";
+        newStuff.innerHTML += "Number of followers: " + j.followers + "</br>";
+        console.log(j.followers_url);
+
+    })
