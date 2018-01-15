1be0e667bf01e04a76da3a228958a7e94d2750d7
Peter Cruckshank <petecruckshank@gmail.com>
2018-01-15 18:50:58 -0500

    put the second fetch call inside of getUser() to return followers

--- JSON Project/jsonhandling.js
+++ JSON Project/jsonhandling.js
@@ -1,37 +1,39 @@
 var followers, searchData;
 
-document.getElementById("button").addEventListener("click", function (r) {
-  getUser(document.getElementById("GitUser").value) ;
-  
+document.getElementById("button").addEventListener("click", function(r) {
+    getUser(document.getElementById("GitUser").value);
+
 })
 
-function getUser (user) {
+function getUser(user) {
 
 
-fetch('https://api.github.com/users/' + user)
-    .then(function(r) {
-        return r.json();
-    })
-    .then(function(j) {
-        console.dir(j);
-        var newStuff = document.getElementById('username');
-        document.getElementById("image").src = j.avatar_url;
-        newStuff.innerHTML += "Real Name: " + j.name + "</br>";
-        newStuff.innerHTML += "Location: " + j.location + "</br>";
-        newStuff.innerHTML += "Bio: " + j.bio + "</br>";
-        newStuff.innerHTML += "Number of followers: " + j.followers + "</br>";
-        followers = j.followers_url;
-        console.log(followers);
+    fetch('https://api.github.com/users/' + user)
+        .then(function(r) {
+            return r.json();
+        })
+        .then(function(j) {
+            console.dir(j);
+            var newStuff = document.getElementById('username');
+            document.getElementById("image").src = j.avatar_url;
+            newStuff.innerHTML += "Real Name: " + j.name + "</br>";
+            newStuff.innerHTML += "Location: " + j.location + "</br>";
+            newStuff.innerHTML += "Bio: " + j.bio + "</br>";
+            newStuff.innerHTML += "Number of followers: " + j.followers + "</br>";
+            followers = j;
+            console.log(followers);
 
 
-    }) // end of the first GitHub fetch request
-  } // end of getUser
+        }) // end of the first GitHub fetch request
 
 
-    // fetch("https://api.github.com/users/petercr/followers")
-    //   .then( function(r) {
-    //     return r.json;
-    //   })
-    //   .then( function(r) {
-    //     console.log(r);
-    //   })
+
+    fetch(`https://api.github.com/users/${user}/followers`)
+        .then(function(x) {
+            return x.json;
+        })
+        .then(function(y) {
+            console.log(y);
+        })
+
+} // end of getUser
