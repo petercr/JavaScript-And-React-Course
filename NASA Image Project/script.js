c4a92560b3acdf8043a7e6d6857aaca457e416e3
Peter Cruckshank <petecruckshank@gmail.com>
2018-01-29 14:48:12 -0500

    added in function to fetch the data from NASA API and return JSON

--- NASA Image Project/script.js
+++ NASA Image Project/script.js
@@ -1,5 +1,15 @@
 // add event listen for the button and assign the text box value to a variable 
 document.querySelector(".btn").addEventListener("click", function() {
     let search = document.getElementById("searchText").value;
-    console.log(search);
+    getSearchData(search);
 })
+
+function getSearchData(searchString) {
+    fetch("https://images-api.nasa.gov/search?q=moon")
+        .then(req => {
+            return req.json();
+        })
+        .then(res => {
+            console.dir(res);
+        })
+}
