0ff8a7c611e5a16df574589adbd1a4638a6d81ec
Peter Cruckshank <petecruckshank@gmail.com>
2018-02-03 15:00:19 -0500

    added in object destructuring for the NASA API response. Also changed the for loop to start at 1 so it wouldn't get the collection data

--- NASA Image Project/script.js
+++ NASA Image Project/script.js
@@ -1,4 +1,4 @@
-// add event listen for the button and assign the text box value to a variable 
+// add event listen for the button and assign the text box value to a variable
 document.querySelector(".btn").addEventListener("click", function() {
     let search = document.getElementById("searchText").value;
     getSearchData(search);
@@ -7,7 +7,7 @@ document.querySelector(".btn").addEventListener("click", function() {
 document.getElementById("searchText").addEventListener("keydown", function (e) {
     if (13 == e.keyCode) {
         let search = document.getElementById("searchText").value;
-        getSearchData(search);  
+        getSearchData(search);
     }
 })
 
@@ -23,30 +23,35 @@ function getSearchData(searchString) {
             let description = res.collection.items[1].data["0"].description; // get the description of the image
             console.log(url + "\n" + description);
 
-            
+
             // for loop to create 10 new image cards and append to #searchResults
 
             // just create a template literal and add it to .innerHTML of the div
-            for (var i = 0; i < 12; i++) {
+            for (var i = 1; i < 13; i++) {
+                let { href } = res.collection.items[i].links["0"];
+                let { description_508 } = res.collection.items[i].data["0"];
+                console.log(href + ' ' + description_508);
+
+
                 let pictureBio = document.createElement("p");
                 pictureBio.classList.add("bio");
                 pictureBio.innerHTML = `A picture of: ${res.collection.items[i].data["0"].description_508}`;
-               
-                
+
+
                 let nasaImages = document.createElement("img");
                 nasaImages.src = res.collection.items[i].links["0"].href;
-                
+
 
                 document.getElementById("searchResults").appendChild(pictureBio);
                 document.getElementById("searchResults").appendChild(nasaImages);
             }
-            
-            
-            
+
+
+
             // let nasa = document.createElement("img");
             // nasa.src = res.collection.items[1].links["0"].href;
             // document.getElementById("searchResults").appendChild(nasa);
-            
+
 
 
         })
