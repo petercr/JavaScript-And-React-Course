8c6499f3b2a41c34d508f7a851d7952a4bd3cd8a
Peter Cruckshank <petecruckshank@gmail.com>
2018-02-03 22:47:32 -0500

    added uglify task to grunt for *.js & *.css this is the end of premade instructions Additional Task is to add in babel and change everything to ES6. Then have babel put ES5 into /dist/

--- Grunt-Project/Gruntfile.js
+++ Grunt-Project/Gruntfile.js
@@ -1,7 +1,26 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        name: "Rufus"
+        name: "Rufus",
+        watch: {
+            files: ["*.js", "*.css"],
+            task: "updated"
+        },
+        uglify: {
+            build: {
+                src: ['index.js', 'logger.js'],
+                dest: 'dist/bundle.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+    grunt.registerTask("updated", () => {
+        grunt.log.writeln(`Yah it updated baby!! ;-)`);
     });
 
     grunt.registerTask("default", () => {
