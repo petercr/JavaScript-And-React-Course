e47921237bb838074ac20ad51d2146d2edd948b8
Peter Cruckshank <petecruckshank@gmail.com>
2018-02-05 22:00:08 -0500

    finished the tutorial part of gulp JS. Now on to project part

--- Gulp-Project/gulpfile.js
+++ Gulp-Project/gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
 
 gulp.task('default', () => {
     console.log('Hello Rufus ;-0');
+});
+
+gulp.task('html', () => {
+    return gulp.src('index.hbs')
+        .pipe(handlebars({}, {
+            batch: ['./partials']
+        }))
+        .pipe(rename({
+            extname: '.html'
+        }))
+        .pipe(gulp.dest('./'));
 });
