32637617ccd5fe4e6335d0d27852d8c528da8fed
Peter Cruckshank <petecruckshank@gmail.com>
2018-02-06 16:40:48 -0500

    Got Gulp to minify the *.js files and place them in the root

--- Gulp-Project/gulpfile.js
+++ Gulp-Project/gulpfile.js
@@ -1,11 +1,22 @@
 const gulp = require('gulp');
 const handlebars = require('gulp-compile-handlebars');
 const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
+
+// default task to be run for Gulp
 gulp.task('default', () => {
     console.log('Hello Rufus ;-0');
 });
 
+
+gulp.task('minify', () => {
+    gulp.src('./src/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./'));
+});
+
+// takes in .hbs and converts it to .html
 gulp.task('html', () => {
     return gulp.src('index.hbs')
         .pipe(handlebars({}, {
---
+++ Gulp-Project/index.js
@@ -0,0 +1,1 @@
+console.log("Hello number 1"),console.log("Hello number 2"),console.log("Hello number 3");
