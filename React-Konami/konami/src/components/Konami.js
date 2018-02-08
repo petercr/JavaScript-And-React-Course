70f551e6e6d966c4c54c53bae928ab45af7d3733
Peter <petecruckshank@gmail.com>
2018-02-07 21:32:32 -0500

    changed app.js to include the konami component

---
+++ React-Konami/konami/src/components/Konami.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Konami extends Component {
+  render() {
+    return (
+      <div>Welcome to my Craxy React App</div>
+    );
+  }
+}
+
+export default Konami;
