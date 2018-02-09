9c89e313e8acbde6ebfee9c1bd187bcdc9e35f07
Peter Cruckshank <petecruckshank@gmail.com>
2018-02-08 21:58:40 -0500

    added key binding to pop up alert

--- React-Konami/konami/src/components/Konami.js
+++ React-Konami/konami/src/components/Konami.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-
+import Mousetrap from 'mousetrap';
 
 class Konami extends Component {
-  render() {
-    return (
-      <div>Welcome to my Craxy React App</div>
-    );
-  }
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+        Mousetrap.bind(['1 2 3 4', 'up up down down left right left right b a'], this.popUp);
+    }
+
+    componentWillUnmount() {
+        Mousetrap.unbind(['1 2 3 4', 'up up down down left right left right b a']);
+
+    }
+
+    popUp() {
+        alert('It worked buddy ;-)');
+    }
+
+    render() {
+        return ( < div > Welcome to my Craxy React App < /div>
+
+        );
+    }
 }
 
 export default Konami;
