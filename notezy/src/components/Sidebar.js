934f08807819c5fbcb29064bf0939884eed9f575
Peter Cruckshank <petecruckshank@gmail.com>
2018-02-28 23:49:17 -0500

    put in stylings and added a btn to add notes

---
+++ notezy/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Editor extends Component {
+  render() {
+    return (
+      <div className="editor column column-25">
+        <button className="button button-clear">+ New Note</button>
+      </div>
+    );
+  }
+}
+
+export default Editor;
