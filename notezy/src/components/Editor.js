32e30290637e6d766f327e921c5e216c3a1d1db0
Peter Cruckshank <petecruckshank@gmail.com>
2018-02-28 23:48:38 -0500

    put in the mde-editor and added onChange function

---
+++ notezy/src/components/Editor.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import SimpleMDE from 'react-simplemde-editor';
+
+
+class Editor extends Component {
+  render() {
+    return (
+      <div className="editor column column-75">
+        <SimpleMDE 
+            onChange = {this.props.change}
+            value = "hello"
+            options = {{
+                autofocus: true
+            }}
+        />
+      </div>
+    );
+  }
+}
+
+export default Editor;
