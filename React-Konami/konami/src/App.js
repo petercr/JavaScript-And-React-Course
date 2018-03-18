dd2735f844c8b857964e16e1ed62da0912748f3a
Peter Cruckshank <petecruckshank@gmail.com>
2018-03-18 17:23:02 -0400

    added state + toggleActivation( ) and handleChange(e) also aded binding to this.toggleAct and this.handleChange

--- React-Konami/konami/src/App.js
+++ React-Konami/konami/src/App.js
@@ -4,17 +4,47 @@ import './App.css';
 import Konami from './components/Konami';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      activated: false,
+      name: ''
+    };
+    this.toggleActivation = this.toggleActivation.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  toggleActivation() {
+    if (this.state.activated === true) {
+      this.setState({ activated: false });
+    }
+    else {
+      this.setState({ activated: true });
+    }
+  }
+
+  handleChange(e) {
+    console.log(e.target.value);
+    this.setState({ name: e.target.value});
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome`{this.state.name}` </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        < Konami />
+        
+        <input type="text" onChange={ this.handleChange} placeholder="Your name" />
+
+        <div className="konami">
+          <button onClick={this.toggleActivation}>Toggle Konami Code</button>
+          {this.state.activated ? <Konami name={this.state.name} /> : <div>Konami Code: Deactivated </div> }
+        
       </div>
     );
   }
