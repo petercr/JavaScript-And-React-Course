3678a179e76e9ad537a6aa6b1fe4278ce24f041d
Peter Cruckshank <petecruckshank@gmail.com>
2018-03-24 18:34:52 -0400

    started to add this.state and handle localStorage

--- notezy/src/App.js
+++ notezy/src/App.js
@@ -5,6 +5,18 @@ import Sidebar from './components/Sidebar';
 import Editor from './components/Editor';
 
 class App extends Component {
+  constructor(){
+    super();
+    const indexKey = 'markdown';
+    const notes = JSON.parse(localStorage(notes));
+
+    this.state = {
+      savedNotes: notes,
+    }
+
+  }
+
+
   render() {
     return (
       <div className="App container">
