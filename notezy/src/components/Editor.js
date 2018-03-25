f37684fc418a8db178ccd51a4edb2fd27645757c
Peter Cruckshank <petecruckshank@gmail.com>
2018-03-25 09:31:34 -0400

    changed starting value

--- notezy/src/components/Editor.js
+++ notezy/src/components/Editor.js
@@ -8,7 +8,7 @@ class Editor extends Component {
       <div className="editor column column-75">
         <SimpleMDE 
             onChange = {this.props.change}
-            value = "hello"
+            value = "What's going on??"
             options = {{
                 autofocus: true
             }}
