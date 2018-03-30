6a4fb2ccfe848842304cd2e7bb2bbbebc6fb3c36
Peter <petecruckshank@gmail.com>
2018-03-29 21:26:33 -0400

    added in new functions to handle saving & updating Not sure if it all works yet, but there's progress ;-)

--- notezy/src/App.js
+++ notezy/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'milligram';
+const uuidv1 = require('uuid/v1');
 
 import Sidebar from './components/Sidebar';
 import Editor from './components/Editor';
@@ -15,6 +16,38 @@ class App extends Component {
       currentNote: ''
     }
 
+    this.noteID = this.noteID.bind(this);
+  }
+
+  noteID() {
+    return uuidv1();
+  }
+
+  addNote() {
+    const newID = this.noteID();
+    const note = {id: newID, body: `What's going on today?`};
+    const newNote = this.state.notes;
+    newNote.push(note);
+    this.setState({notes: newNote});
+
+  }
+
+  selectNote(note) {
+    if (note === this.state.selectedNote) return;
+    this.setState({selectedNote: note});
+  }
+
+  save(notes) {
+    if (!notes) return;
+    const key = 'markdown';
+    localStorage.setItem(key, JSON.stringify(notes));
+  }
+
+  updateNote(body) {
+    let notes =this.state.notes;
+    let currentNote = this.state.selectedNote;
+    currentNote.body = body;
+    this.setState({selectedNote: currentNote});
   }
 
 
