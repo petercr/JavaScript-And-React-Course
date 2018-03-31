6c0d1740354b7c36b33276d0d5ae512b491bbc89
Peter <petecruckshank@gmail.com>
2018-03-30 21:59:48 -0400

    added renderList() to make LI s for all notes added a UL tag and called renderList() inside it

--- notezy/src/components/Sidebar.js
+++ notezy/src/components/Sidebar.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 
-class Editor extends Component {
+class Sidebar extends Component {
+  renderList() {
+    let noteList = [];
+    this.props.notes.forEach((note) => {
+      list.push(
+        <li key={note.id} onClick={() => this.props.select(note)} className={note.id === this.props.currentNote.id ? 'selected' : ''}>
+          { note.body }
+        </li>
+      );
+    });
+    return list;
+  }
+
   render() {
     return (
       <div className="editor column column-25">
-        <button className="button button-clear">+ New Note</button>
+        <button className="button button-clear" onClick={this.props.addNote} >+ New Note </button>
+        <ul className="list">
+          {this.renderList()}
+        </ul>
       </div>
     );
   }
 }
 
-export default Editor;
+export default Sidebar;
