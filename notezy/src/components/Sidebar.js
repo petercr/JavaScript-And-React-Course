d057ceb559ac72acbaaafae51f2808714a416c41
Peter Cruckshank <petecruckshank@gmail.com>
2018-03-31 19:19:44 -0400

    fixed .forEach() loop and button call for props

--- notezy/src/components/Sidebar.js
+++ notezy/src/components/Sidebar.js
@@ -5,19 +5,19 @@ class Sidebar extends Component {
   renderList() {
     let noteList = [];
     this.props.notes.forEach((note) => {
-      list.push(
+      noteList.push(
         <li key={note.id} onClick={() => this.props.select(note)} className={note.id === this.props.currentNote.id ? 'selected' : ''}>
           { note.body }
         </li>
       );
     });
-    return list;
+    return noteList;
   }
 
   render() {
     return (
       <div className="editor column column-25">
-        <button className="button button-clear" onClick={this.props.addNote} >+ New Note </button>
+        <button className="button button-clear" onClick={this.props.add} >+ New Note </button>
         <ul className="list">
           {this.renderList()}
         </ul>
